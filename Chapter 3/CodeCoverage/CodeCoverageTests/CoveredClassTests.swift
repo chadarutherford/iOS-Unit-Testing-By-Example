4e14c74293886e7f851dfdfb4c3fba9a89fa705a
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-24 12:44:22 -0400

    Added code coverage for sequential statements

--- Chapter 3/CodeCoverage/CodeCoverageTests/CoveredClassTests.swift
+++ Chapter 3/CodeCoverage/CodeCoverageTests/CoveredClassTests.swift
@@ -19,4 +19,20 @@ class CoveredClassTests: XCTestCase {
 		let result = CoveredClass.max(3, 2)
 		XCTAssertEqual(result, 3)
 	}
+	
+	func test_commaSeparated_from2To4_shouldReturn234SeparatedByCommas() {
+		let result = CoveredClass.commaSeparated(from: 2, to: 4)
+		XCTAssertEqual(result, "2,3,4")
+	}
+	
+	func test_commaSeparated_from2To2_shouldReturn2WithNoComma() {
+		let result = CoveredClass.commaSeparated(from: 2, to: 2)
+		XCTAssertEqual(result, "2")
+	}
+	
+	func test_area_withWidth7_shouldBe49() {
+		let sut = CoveredClass()
+		sut.width = 7
+		XCTAssertEqual(sut.area, 49)
+	}
 }
