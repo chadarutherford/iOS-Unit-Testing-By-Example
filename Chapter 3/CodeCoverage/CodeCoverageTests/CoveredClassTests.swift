0c5426e08f6c5430fc1d108c212bf7b4fc7ef7aa
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-24 12:17:03 -0400

    Added partial coverage for max function

--- Chapter 3/CodeCoverage/CodeCoverageTests/CoveredClassTests.swift
+++ Chapter 3/CodeCoverage/CodeCoverageTests/CoveredClassTests.swift
@@ -6,10 +6,11 @@
 //
 
 import XCTest
+@testable import CodeCoverage
 
 class CoveredClassTests: XCTestCase {
 	
-	func test_zero() {
-		XCTFail("Tests not yet implemented in CoveredClassTests")
+	func test_max_with1And2_shouldReturnSomething() {
+		let result = CoveredClass.max(1, 2)
 	}
 }
