801a0cb06c75639e5caf9f09ad2bdaa714ff1472
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-24 11:27:24 -0400

    Incorrect creation of class inside test to reduce duplication

--- Chapter 1/LifeCycle/LifeCycleTests/MyClassTests.swift
+++ Chapter 1/LifeCycle/LifeCycleTests/MyClassTests.swift
@@ -6,7 +6,22 @@
 //
 
 import XCTest
+@testable import LifeCycle
 
 class MyClassTests: XCTestCase {
 	
+	// Test Zero
+//	func test_zero() {
+//		XCTFail("Tests not yet implemented in MyClassTests")
+//	}
+	
+	private let sut = MyClass()
+	
+	func test_methodOne() {
+		sut.methodOne()
+	}
+	
+	func test_methodTwo() {
+		sut.methodTwo()
+	}
 }
