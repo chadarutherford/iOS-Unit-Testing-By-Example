a51e4b3f5030c370ebb393a737668b1006e30a20
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-24 16:40:41 -0400

    Set up blank test suite

---
+++ Chapter 7/OutletConnections/OutletConnections/Controller/OutletConnectionsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OutletConnectionsViewController.swift
+//  OutletConnections
+//
+//  Created by Chad Rutherford on 9/24/20.
+//
+
+import UIKit
+
+class OutletConnectionsViewController: UIViewController {
+	
+	@IBOutlet private weak var label: UILabel!
+	@IBOutlet private weak var button: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Chapter 7/OutletConnections/OutletConnectionsTests/OutletConnectionsViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  OutletConnectionsViewControllerTests.swift
+//  OutletConnectionsTests
+//
+//  Created by Chad Rutherford on 9/24/20.
+//
+
+import XCTest
+@testable import OutletConnections
+
+class OutletConnectionsViewControllerTests: XCTestCase {
+	
+	
+}
