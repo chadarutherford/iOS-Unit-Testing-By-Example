368cd5db96715499025e0bfeb5c17b41c96e5b86
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-24 17:42:31 -0400

    Added helper methods for button taps

--- Chapter 7/ButtonTap/ButtonTapTests/ViewControllerTests.swift
+++ Chapter 7/ButtonTap/ButtonTapTests/ViewControllerTests.swift
@@ -14,6 +14,6 @@ class ViewControllerTests: XCTestCase {
 		let storyboard = UIStoryboard(name: "Main", bundle: nil)
 		let sut: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
 		sut.loadViewIfNeeded()
-		sut.button.sendActions(for: .touchUpInside)
+		sut.button.tap()
 	}
 }
