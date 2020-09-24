30d505bfc650d9bfe2a869989aa1f94a347fb439
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-24 11:08:13 -0400

    Added LifeCycle Project

---
+++ Chapter 1/LifeCycle/LifeCycle/Model/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  LifeCycle
+//
+//  Created by Chad Rutherford on 9/24/20.
+//
+
+import Foundation
+
+class MyClass {
+	func methodOne() {
+		print(">> methodOne")
+	}
+	
+	func methodTwo() {
+		print(">> methodTwo")
+	}
+}
---
+++ Chapter 1/LifeCycle/LifeCycleTests/MyClassTests.swift
@@ -0,0 +1,12 @@
+//
+//  MyClassTests.swift
+//  LifeCycleTests
+//
+//  Created by Chad Rutherford on 9/24/20.
+//
+
+import XCTest
+
+class MyClassTests: XCTestCase {
+	
+}
