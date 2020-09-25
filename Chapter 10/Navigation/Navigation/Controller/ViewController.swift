97551f23bda137518e4881c6062f16d5dc76fce2
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-25 07:50:55 -0400

    Added Code based navigation for testing

--- Chapter 10/Navigation/Navigation/Controller/ViewController.swift
+++ Chapter 10/Navigation/Navigation/Controller/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet private(set) weak var codePushButton: UIButton!
+	@IBOutlet private(set) weak var codeModalButton: UIButton!
+	@IBOutlet private(set) weak var seguePushButton: UIButton!
+	@IBOutlet private(set) weak var segueModalButton: UIButton!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
-
-
+	
+	@IBAction func codePushTapped(_ sender: UIButton) {
+		let nextVC = CodeNextViewController(labelText: "Pushed from code")
+		self.navigationController?.pushViewController(nextVC, animated: true)
+	}
+	
+	@IBAction func codeModalTapped(_ sender: UIButton) {
+		let nextVC = CodeNextViewController(labelText: "Modal from code")
+		present(nextVC, animated: true)
+	}
+	
+	@IBAction func seguePushTapped(_ sender: UIButton) {
+		
+	}
+	
+	@IBAction func segueModalTapped(_ sender: UIButton) {
+		
+	}
 }
-
