f9439ff868fac206969b21e571d006eb04875f6c
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-25 09:05:14 -0400

    Added initial setup using UserDefaults

--- Chapter 11/UserDefaults/UserDefaults/Controller/ViewController.swift
+++ Chapter 11/UserDefaults/UserDefaults/Controller/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet private(set) weak var counterLabel: UILabel!
+	@IBOutlet private(set) weak var incrementButton: UIButton!
+	
+	private var count = 0 {
+		didSet {
+			counterLabel.text = "\(count)"
+			UserDefaults.standard.set(count, forKey: "count")
+		}
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		count = UserDefaults.standard.integer(forKey: "count")
 	}
-
-
+	
+	@IBAction private func incrementButtonTapped(_ sender: UIButton) {
+		count += 1
+	}
 }
-
