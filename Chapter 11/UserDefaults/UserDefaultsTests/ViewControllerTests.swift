d5ad7c711a0828df38ebd89fc57ad0df6d4c73bb
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-25 10:05:22 -0400

    Added tests to verify writing to UserDefaults and incrementing counter passes

--- Chapter 11/UserDefaults/UserDefaultsTests/ViewControllerTests.swift
+++ Chapter 11/UserDefaults/UserDefaultsTests/ViewControllerTests.swift
@@ -32,7 +32,19 @@ class ViewControllerTests: XCTestCase {
 		XCTAssertEqual(sut.counterLabel.text, "7", "counter label")
 	}
 	
+	func test_tappingButton_with12InUserDefaults_shouldWrite13ToUserDefaults() {
+		userDefaults.integers = ["count": 12]
+		sut.loadViewIfNeeded()
+		sut.incrementButton.tap()
+		XCTAssertEqual(userDefaults.integers["count"], 13)
+	}
 	
+	func test_tappingButton_with42InUserDefaults_shouldShow43InCounterLabel() {
+		userDefaults.integers = ["count": 42]
+		sut.loadViewIfNeeded()
+		sut.incrementButton.tap()
+		XCTAssertEqual(sut.counterLabel.text, "43")
+	}
 	
 	override func tearDown() {
 		sut = nil
