d7e5c7c1dc9b470e03d570cf0f002e25252a388b
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-25 10:23:52 -0400

    Added and network call and manually verified it works

--- Chapter 12/NetworkRequest/NetworkRequest/Controller/ViewController.swift
+++ Chapter 12/NetworkRequest/NetworkRequest/Controller/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet private(set) weak var button: UIButton!
+	
+	private var dataTask: URLSessionDataTask?
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
-
-
+	
+	private func searchForBook(terms: String) {
+		guard let encodedTerms = terms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+			  let url = URL(string: "https://itunes.apple.com/search?"
+								+ "media=ebook&term=\(encodedTerms)") else { return }
+		let request = URLRequest(url: url)
+		dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+			guard let self = self else { return }
+			let decoded = String(data: data ?? Data(), encoding: .utf8)
+			print("response: \(String(describing: response))")
+			print("data: \(String(describing: decoded))")
+			print("error: \(String(describing: error))")
+			
+			DispatchQueue.main.async { [weak self] in
+				guard let self = self else { return }
+				self.dataTask = nil
+				self.button.isEnabled = true
+			}
+		}
+		button.isEnabled = false
+		dataTask?.resume()
+	}
+	
+	@IBAction private func buttonTapped(_ sender: UIButton) {
+		searchForBook(terms: "out from boneville")
+	}
 }
-
