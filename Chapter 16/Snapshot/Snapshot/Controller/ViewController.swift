2c4972d223ec4edf980b770199174d2d393f13bf
Chad Rutherford <chad_rutherford@outlook.com>
2020-09-29 12:12:50 -0400

    Added Snapshot project

---
+++ Chapter 16/Snapshot/Snapshot/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapshot
+//
+//  Created by Chad Rutherford on 9/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
